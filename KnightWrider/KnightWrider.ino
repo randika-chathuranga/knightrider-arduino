d6c7332fbc69c9eea90e9e9a57b34b4838bce5da
randika-chathuranga <56346427+randika-chathuranga@users.noreply.github.com>
2019-10-09 04:28:35 -0700

    Add files via upload

---
+++ KnightWrider/KnightWrider.ino
@@ -0,0 +1,92 @@
+int led1=2;
+int led2=3;
+int led3=4;
+int led4=5;
+int led5=6;
+int led6=7;
+int led7=8;
+int led8=9;
+int led9=10;
+
+
+void setup() {
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+pinMode(led5, OUTPUT);
+pinMode(led6, OUTPUT);
+pinMode(led7, OUTPUT);
+pinMode(led8, OUTPUT);
+pinMode(led9, OUTPUT); 
+
+}
+
+void loop() {
+ digitalWrite(led1, HIGH);
+ delay(50);
+ digitalWrite(led1, LOW);
+  delay(50);
+  digitalWrite(led2, HIGH);
+  delay(50);
+ digitalWrite(led2, LOW);
+  delay(50);
+  digitalWrite(led3, HIGH);
+ delay(50);
+ digitalWrite(led3, LOW);
+  delay(50);
+  digitalWrite(led4, HIGH);
+ delay(50);
+ digitalWrite(led4, LOW);
+  delay(50);
+  digitalWrite(led5, HIGH);
+ delay(50);
+ digitalWrite(led5, LOW);
+  delay(50);
+  digitalWrite(led6, HIGH);
+ delay(50);
+ digitalWrite(led6, LOW);
+  delay(50);
+  digitalWrite(led7, HIGH);
+ delay(50);
+ digitalWrite(led7, LOW);
+  delay(50);
+  digitalWrite(led8, HIGH);
+ delay(50);
+ digitalWrite(led8, LOW);
+  delay(50);
+  digitalWrite(led9, HIGH);
+ delay(50);
+ digitalWrite(led9, LOW);
+  delay(50);
+
+
+  digitalWrite(led8, HIGH);
+ delay(50);
+ digitalWrite(led8, LOW);
+   delay(50);
+  digitalWrite(led7, HIGH);
+ delay(50);
+ digitalWrite(led7, LOW);
+   delay(50);
+  digitalWrite(led6, HIGH);
+ delay(50);
+ digitalWrite(led6, LOW);
+   delay(50);
+  digitalWrite(led5, HIGH);
+ delay(50);
+ digitalWrite(led5, LOW);
+   delay(50);
+  digitalWrite(led4, HIGH);
+ delay(50);
+ digitalWrite(led4, LOW);
+   delay(50);
+  digitalWrite(led3, HIGH);
+ delay(50);
+ digitalWrite(led3, LOW);
+   delay(50);
+  digitalWrite(led2, HIGH);
+  delay(50);
+ digitalWrite(led2, LOW);
+ 
+}
